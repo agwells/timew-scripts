d77eb56d7ab1863fe73e264d3cd076568a27a58e
Aaron Wells <aaronw@catalyst.net.nz>
2023-11-06 10:47:01 +1300

    Make the panel status widget blink when no task is set

--- timew-panel-status.bash
+++ timew-panel-status.bash
@@ -1,3 +1,14 @@
 #!/bin/bash
 
-timew | head -n 1
+if $(timew >> /dev/null); then
+    timew | head -n 1
+else
+    # Make it blink into ALL CAPS to try to remind
+    # me to set a task.
+    MSG=$(timew)
+    if expr $(date +%s | tail -c 2) % 2 >> /dev/null; then
+        echo $MSG
+    else
+        echo ${MSG^^}
+    fi
+fi
